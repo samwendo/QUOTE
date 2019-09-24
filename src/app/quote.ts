8b1f4b451a75ca20898ac91e170af3853b2fbf40
samwendo <wendosam21@gmail.com>
2019-09-24 11:55:58 +0300

    add quote ts

---
+++ src/app/quote.ts
@@ -0,0 +1,13 @@
+export class Quote {
+ showDescription: boolean;
+ constructor(
+   public id: number,
+   public name: string,
+   public user: string,
+   public author: string,
+   public completeDate: Date,
+   public upvotes: number,
+   public downvotes: number) {
+   this.showDescription = false;
+   }
+}
